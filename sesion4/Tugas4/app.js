b3a3b9d060fc3b748d9a95e66dc4f334fd1f472d
zainurrqn <zainurroiqin3010@gmail.com>
2023-08-08 01:05:43 +0700

    update

---
+++ sesion4/Tugas4/app.js
@@ -0,0 +1,14 @@
+// Deklarasi dan inisialisasi variabel
+const diameter = 14;
+const jariJari = diameter / 2;
+const phi = 3.14159;
+
+// Menghitung luas lingkaran
+const luas = phi * jariJari * jariJari;
+
+// Menghitung keliling lingkaran
+const keliling = 2 * phi * jariJari;
+
+// Menampilkan hasil
+console.log("Luas Lingkaran: " + luas);
+console.log("Keliling Lingkaran: " + keliling);
